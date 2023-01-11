75a01debc7f011069ef434abd6a9c623e0be64e9
Melike <yaylamelike@gmail.com>
2023-01-12 00:08:03 +0300

    gestureRecognizer

--- ArtBookProject/ArtBookProject/DetailsVC.swift
+++ ArtBookProject/ArtBookProject/DetailsVC.swift
@@ -18,11 +18,15 @@ class DetailsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
     }
     
-
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
+    
     @IBAction func saveButtonClicked(_ sender: Any) {
     }
     
