6bb89b1168b97ed185675e186710d80f814de5c8
Melike <yaylamelike@gmail.com>
2023-01-23 12:57:43 +0300

    ToDoList

--- ArtBookProject/ArtBookProject/ViewController.swift
+++ ArtBookProject/ArtBookProject/ViewController.swift
@@ -6,21 +6,66 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        getData()
+    }
+    
+    func getData() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let result = try context.fetch(fetchRequest)
+            for result in result as! [NSManagedObject] {
+                if let name = result.value(forKey: "name") as? String {
+                    self.nameArray.append(name)
+                }
+                if let id = result.value(forKey:"id") as? UUID{
+                    self.idArray.append(id)
+                }
+                self.tableView.reloadData()
+            }
+            
+            }catch {
+            print("error")
+        }
     }
 
-
+    
+    
     
     @objc func addButtonClicked(){
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
     }
 }
 
---
+++ ToDoListApp/ToDoListApp/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoListApp
+//
+//  Created by Melike Yayla on 22.01.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
