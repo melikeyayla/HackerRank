123e95be2cbc4ae3939512c6326daa8a4b6f0af2
Melike <yaylamelike@gmail.com>
2023-03-19 19:21:30 +0300

    AngryBird

--- AngryBirdClone/AngryBirdClone/GameViewController.swift
+++ AngryBirdClone/AngryBirdClone/GameViewController.swift
@@ -25,9 +25,7 @@ class GameViewController: UIViewController {
             }
             
             view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsPhysics = true
         }
     }
 
